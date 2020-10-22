10b2b0ca3319619d8885a6af00d453e7b8acb416
Mayko Kioschi <mkioschi@gmail.com>
2020-10-22 00:01:21 -0300

    Commit #30

--- .docker/bin/register-ssl.sh
+++ .docker/bin/register-ssl.sh
@@ -55,9 +55,6 @@ do
     shift
 done
 
-echo $docker_compose;
-exit
-
 # Valida variáveis obrigatórias
 if [ -z "$docker_compose" ]; then
     echo; echo "[error] O prefixo do docker-compose é obrigatório."; echo;
